e0fbdb9e28f235b4a960f4c19fce73a8f0e42857
ZuriGE <zurigales@gmail.com>
2023-10-18 13:47:37 +0200

    se añaden retos opcionales dia 1

--- POO/opcional_matriz.js
+++ POO/opcional_matriz.js
@@ -17,87 +17,62 @@ class Matrix {
 
     add (m) {
         let result;
-        let condit = true;
-        let mSum = m.myMatrix
-    
-        console.log(this.myMatrix.length);
-        console.log( mSum.length);
+        let condit = true
+        
+        console.log(m.length)
+        console.log(this.myMatrix.length)
 
-
-        if (mSum.length == this.myMatrix.length){
-            result = [];
-            this.myMatrix.forEach((vector, index)=>{
-                mSum[index].__proto__ = classVector.prototype;
-                vector.__proto__ = classVector.prototype;
-               mSum[index].add(vector)
-            })
+        if (m.length != this.myMatrix.length) {
+            condit = false    
         }else{
-            result = 'no se cumplen las condiciones'
+            result = this.myMatrix.map((valor, i)=>{
+                if (valor.length == m[i].length){
+                    return valor.map((valor, j) => valor + m[i][j])
+                }else{
+                    condit = false
+                }
+            })   
         }
-        !condit ? result = 'no se cumplen las condiciones': result
-        return result
+        
+        if (condit) {
+            return result
+        }else{
+            return 'no se cumplen las condiciones'
+        }
+    }
+
+    productNum(n){
+        return this.myMatrix.map((value) => value.map((value) => n*value));
+
+    }
+        
+    
+
+    subs (m) {
+        let result
+        let condit = true
+
+        if (m.length == this.myMatrix.length) {
+            result = this.myMatrix.map((valor, i)=>{
+                if (valor.length == m[i].length){
+                    return valor.map((valor, j) => m[i][j] - valor)
+                }else{
+                    condit = false
+                }
+            })       
+        }else{
+            condit = false;
+        }
+        if (condit) {
+            return result
+        }else{
+            return 'no se cumplen las condiciones'
+        }
     }
     
-    // //ProductoNumeroMatriz(n, m): Multiplica la matriz m por el numero n.
     
-    // const productoNumeroMatriz = (n, m) => m.map(value => myLib.productoNumeroVector(n, value))
-    
-    // // restaMatriz(m1, m2): Resta dos matrices m1 y m2 si y solo si tienen el mismo numero de elementos.
-    
-    // const restaMatriz = (m1, m2) =>{
-    //     let result;
-    //     let condit = true;
-    //     if (m1.length == m2.length){
-    //         result = [];
-    //         for (let i = 0; i<m1.length; i++){
-    //             let myVec = myLib.restaVector(m1[i], m2[i]);
-    //             myVec == 'no se cumplen las condiciones' ? condit = false : result.push(myVec);
-                
-    //         }
-    //     }else{
-    //         result = 'no se cumplen las condiciones'
-    //     }
-    //     !condit ? result = 'no se cumplen las condiciones': result
-    //     return result
-    // }
-    
-    // // Multiplica dos matrices m1 y m2 si y solo si tienen el mismo número de elementos bertb
-    
-    // const productoMatriz = (m1, m2) =>{
-    //     let result;
-    //     let condit = true;
-    //     if (m1.length == m2.length){
-    //         result = [];
-    //         for (let i = 0; i<m1.length; i++){
-    //             let myVec = myLib.productoVector(m1[i], m2[i]);
-    //             myVec == 'no se cumplen las condiciones' ? condit = false : result.push(myVec);
-                
-    //         }
-    //     }else{
-    //         result = 'no se cumplen las condiciones'
-    //     }
-    //     !condit ? result = 'no se cumplen las condiciones': result
-    //     return result
-    // }
-    
-    // module.exports = {crearMatriz,sumaMatriz,productoNumeroMatriz,restaMatriz,productoMatriz}
-
-
-
 
 }
 
-let mx = new Matrix(4,5,10)
-let mx2 = new Matrix(4,5,10)
-let mx3 = new Matrix(4,6,10)
-let mx4 = new Matrix(7,5,10)
-
-console.log(mx)
-console.log(mx2)
-
-
-console.log(mx.add(mx2));
-// console.log(mx.add(mx3));
-// console.log(mx.add(mx4));
-
+module.exports = Matrix;
 
